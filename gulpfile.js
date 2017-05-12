eb5aa1a8f1bc0579c55090baaec8fa40486dbcef
Kamil Kondratowicz <prywatny3@o2.pl>
2017-05-12 22:42:35 +0200

    Added timestamp to JS and CSS

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,15 @@
     const autoprefixer = require('gulp-autoprefixer');
     const cssmin = require('gulp-cssmin');
     const fileinclude = require('gulp-file-include');
-    const concat = require('gulp-concat');
     const closureCompiler = require('gulp-closure-compiler');
     const htmlmin = require('gulp-htmlmin');
     const copy = require('gulp-copy');
     const sitemap = require('gulp-sitemap');
+    const rename = require('gulp-rename');
     
+    const timestamp = Date.now();
+    const css = `main${timestamp}.css`;
+    const js = `scripts${timestamp}.js`;
 
     gulp.task('delFiles', function() {
         return del(['dist']);
@@ -20,11 +23,12 @@
     gulp.task('sass', ['delFiles'], function() {
         return gulp.src('src/sass/main.scss')
             .pipe(sass().on('error', sass.logError))
+            .pipe(rename({suffix: timestamp}))
             .pipe(gulp.dest('dist/css'));
     });
 
     gulp.task('autoprefixer', ['sass'], function() {
-        return gulp.src('dist/css/main.css')
+        return gulp.src('dist/css/*.css')
             .pipe(autoprefixer({
                 browsers: ['last 2 versions'],
                 cascade: false
@@ -42,20 +46,18 @@
         gulp.src(['src/index.html'])
             .pipe(fileinclude({
                 prefix: '@@',
-                basepath: 'src/templates'
+                basepath: 'src/templates',
+                context: {
+                    js: `/js/${js}`,
+                    css: `/css/${css}`
+                }
         }))
         .pipe(gulp.dest('dist'));
     });
     
-    gulp.task('concat', ['include'], function() {
-        return gulp.src('src/js/**')
-            .pipe(concat('scripts.js'))
-            .pipe(gulp.dest('dist/js'));
-    });
-    
-    gulp.task('jsCompiler', ['concat'], function() {
-        return gulp.src('dist/js/scripts.js')
-            .pipe(closureCompiler({fileName: 'scripts.js',
+    gulp.task('jsCompiler', ['include'], function() {
+        return gulp.src('src/js/*.js')
+            .pipe(closureCompiler({fileName: js,
                                    compilerFlags: {
                                        compilation_level: 'SIMPLE_OPTIMIZATIONS',
                                        language_in: 'ECMASCRIPT6_STRICT',
@@ -70,6 +72,7 @@
             .pipe(htmlmin({collapseWhitespace: true}))
             .pipe(gulp.dest('dist'));
     });
+    
     
     gulp.task('copy', ['minifyHTML'], function() {
        return gulp.src(['src/img/**', 'src/*.ico', 'src/*.xml', 'src/*.json'])
